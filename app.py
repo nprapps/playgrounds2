2e878b5a41700cd97df1367cd02caf44df4b91c2
Danny DeBelius <danny.debelius@gmail.com>
2013-06-13 09:41:59 -0400

    Added option to run app server on a port other than 8000.

--- app.py
+++ app.py
@@ -12,6 +12,8 @@ import app_config
 import data
 from render_utils import flatten_app_config, make_context
 
+import argparse
+
 app = Flask(app_config.PROJECT_NAME)
 
 
@@ -125,4 +127,10 @@ def urlencode_filter(s):
     return Markup(s)
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8000, debug=app_config.DEBUG)
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-p', '--port')
+    args = parser.parse_args()
+    server_port = 8000
+    if args.port:
+        server_port = int(args.port)
+    app.run(host='0.0.0.0', port=server_port, debug=app_config.DEBUG)
