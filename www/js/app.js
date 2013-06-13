9a7178123d000fd393f54475e78c78564d94c2bc
Christopher Groskopf <staringmonkey@gmail.com>
2013-06-13 09:54:04 -0500

    Even more code cleanup.

--- www/js/app.js
+++ www/js/app.js
@@ -49,6 +49,65 @@ function cloudSearchToDeg(uint) {
     return parseFloat((uint / APP_CONFIG.CLOUD_SEARCH_DEG_SCALE) - APP_CONFIG.CLOUD_SEARCH_DEG_OFFSET);
 }
 
+function buildCloudSearchParams() {
+    /*
+     * Reads the current state of the UI and builds appropraite CloudSearch query params.
+     */
+    var deployment_target = (APP_CONFIG.DEPLOYMENT_TARGET || 'staging');
+    var query = $search_query.val();
+    var latitude = parseFloat($search_latitude.val());
+    var longitude = parseFloat($search_longitude.val());
+
+    var params = {};
+    var return_fields = ['name', 'city', 'state', 'latitude', 'longitude'];
+
+    var query_bits = ['deployment_target:\'' + deployment_target + '\''];
+
+    if (query) {
+        query_bits.push('full_text:\'' + query + '\'');
+    }
+
+    // If using geosearch
+    if (latitude) {
+        // Generate bounding box for map viewport
+        var point = crs.latLngToPoint(new L.LatLng(latitude, longitude), zoom);
+        var upper_left = point.subtract([RESULTS_MAP_WIDTH / 2, RESULTS_MAP_HEIGHT / 2]);
+        var lower_right = point.add([RESULTS_MAP_WIDTH / 2, RESULTS_MAP_HEIGHT / 2]);
+        var northwest = crs.pointToLatLng(upper_left, zoom);
+        var southeast = crs.pointToLatLng(lower_right, zoom);
+
+        query_bits.push('longitude:' + degToCloudSearch(northwest.lng) + '..' + degToCloudSearch(southeast.lng) + ' latitude:' + degToCloudSearch(southeast.lat) + '..' + degToCloudSearch(northwest.lat));
+
+        var latitude_radians = degToRad(latitude); 
+        var longitude_radians = degToRad(longitude);
+        var offset = APP_CONFIG.CLOUD_SEARCH_DEG_OFFSET;
+        var scale = APP_CONFIG.CLOUD_SEARCH_DEG_SCALE;
+
+        // Compile ranking algorithm (spherical law of cosines)
+        // Note results are scaled up by 1000x.
+        var rank_distance = '3958.761 * Math.acos(Math.sin(' + latitude_radians + ') * Math.sin(((latitude / ' + scale + ') - ' + offset + ') * 3.14159 / 180) + Math.cos(' + latitude_radians + ') * Math.cos(((latitude / ' + scale + ') - ' + offset + ') * 3.14159 / 180) * Math.cos((((longitude / ' + scale + ') - ' + offset + ') * 3.14159 / 180) - ' + longitude_radians + ')) * 1000';
+
+        params['rank'] = 'distance';
+        params['rank-distance'] = rank_distance;
+
+        return_fields.push('distance');
+    } else {
+        params['rank'] = 'name';
+    }
+
+    params['bq'] = '(and ' + query_bits.join(' ') + ')';
+    params['return-fields'] = return_fields.join(',')
+
+    return params;
+}
+
+function buildMapboxPin(size, shape, color, lat, lng) {
+    /*
+     * Generate the URL format for a mapbox static pin.
+     */
+    return 'pin-' + size + '-' + shape + '+' + color + '(' + lng + ',' + lat + ')';
+}
+
 $(function() {
     $search_form = $('#search');
     $search_query = $('#search input[name="query"]');
@@ -61,7 +120,7 @@ $(function() {
     $zoom_in = $('#zoom-in');
     $zoom_out = $('#zoom-out');
 
-    var crs = L.CRS.EPSG3857;
+    crs = L.CRS.EPSG3857;
 
     $geolocate_button.click(function() {
         navigator.geolocation.getCurrentPosition(function(position) {
@@ -116,52 +175,10 @@ $(function() {
     });
 
     $search_form.submit(function() {
-        var deployment_target = (APP_CONFIG.DEPLOYMENT_TARGET || 'staging');
-        var query = $search_query.val();
         var latitude = parseFloat($search_latitude.val());
         var longitude = parseFloat($search_longitude.val());
 
-        var params = {};
-        var return_fields = ['name', 'city', 'state', 'latitude', 'longitude'];
-
-        var query_bits = ['deployment_target:\'' + deployment_target + '\''];
-
-        if (query) {
-            query_bits.push('full_text:\'' + query + '\'');
-        }
-
-        // If using geosearch
-        if (latitude) {
-            // Generate bounding box for map viewport
-            var point = crs.latLngToPoint(new L.LatLng(latitude, longitude), zoom);
-            var upper_left = point.subtract([RESULTS_MAP_WIDTH / 2, RESULTS_MAP_HEIGHT / 2]);
-            var lower_right = point.add([RESULTS_MAP_WIDTH / 2, RESULTS_MAP_HEIGHT / 2]);
-            var northwest = crs.pointToLatLng(upper_left, zoom);
-            var southeast = crs.pointToLatLng(lower_right, zoom);
-
-            query_bits.push('longitude:' + degToCloudSearch(northwest.lng) + '..' + degToCloudSearch(southeast.lng) + ' latitude:' + degToCloudSearch(southeast.lat) + '..' + degToCloudSearch(northwest.lat));
-
-            var latitude_radians = degToRad(latitude); 
-            var longitude_radians = degToRad(longitude);
-            var offset = APP_CONFIG.CLOUD_SEARCH_DEG_OFFSET;
-            var scale = APP_CONFIG.CLOUD_SEARCH_DEG_SCALE;
-
-            // Compile ranking algorithm (spherical law of cosines)
-            // Note results are scaled up by 1000x.
-            var rank_distance = '3958.761 * Math.acos(Math.sin(' + latitude_radians + ') * Math.sin(((latitude / ' + scale + ') - ' + offset + ') * 3.14159 / 180) + Math.cos(' + latitude_radians + ') * Math.cos(((latitude / ' + scale + ') - ' + offset + ') * 3.14159 / 180) * Math.cos((((longitude / ' + scale + ') - ' + offset + ') * 3.14159 / 180) - ' + longitude_radians + ')) * 1000';
-
-            params['rank'] = 'distance';
-            params['rank-distance'] = rank_distance;
-
-            return_fields.push('distance');
-        } else {
-            params['rank'] = 'name';
-        }
-
-        params['bq'] = '(and ' + query_bits.join(' ') + ')';
-        params['return-fields'] = return_fields.join(',')
-
-        $.getJSON('/cloudsearch/2011-02-01/search', params, function(data) {
+        $.getJSON('/cloudsearch/2011-02-01/search', buildCloudSearchParams(), function(data) {
             $search_results.empty();
             $search_results_map_wrapper.hide();
 
@@ -171,21 +188,25 @@ $(function() {
                 _.each(data['hits']['hit'], function(hit, i) {
                     var context = $.extend(APP_CONFIG, hit);
                     context['letter'] = LETTERS[i];
+
                     var html = JST.playground_item(context);
 
                     $search_results.append(html);
 
                     if (hit.data.latitude.length > 0) {
-                        markers.push('pin-m-' + context['letter'] + '+ff6633(' + cloudSearchToDeg(hit.data.longitude[0]) + ',' + cloudSearchToDeg(hit.data.latitude[0]) + ')');
+                        var lat = cloudSearchToDeg(hit.data.latitude[0]);
+                        var lng = cloudSearchToDeg(hit.data.longitude[0]);
+
+                        markers.push(buildMapboxPin('m', context['letter'], 'ff6633', lat, lng));
                     }
                 });
             } else {
                 $search_results.append('<li>No results</li>');
             }
 
-            markers.push('pin-l-circle+006633(' + longitude + ',' + latitude + ')');
+            if (latitude) {
+                markers.push(buildMapboxPin('l', 'circle', '006633', latitude, longitude));
 
-            if (latitude && markers.length > 0) {
                 $search_results_map.attr('src', 'http://api.tiles.mapbox.com/v3/' + APP_CONFIG.MAPBOX_BASE_LAYER + '/' + markers.join(',') + '/' + longitude + ',' + latitude + ',' + zoom + '/' + RESULTS_MAP_WIDTH + 'x' + RESULTS_MAP_HEIGHT + '.png');
 
                 $search_results_map_wrapper.show();
