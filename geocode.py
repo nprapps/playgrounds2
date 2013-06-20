5cd2416d3637d5a4d71fa941a098ed357ce64f0d
Jeremy Bowers <jeremyjbowers@gmail.com>
2013-06-20 16:35:20 -0400

    Updates and geocoding.

--- geocode.py
+++ geocode.py
@@ -1,28 +1,38 @@
+import csv
 import time
 
 from models import Playground
 from geopy import geocoders
 
-p = Playground.select().where(Playground.longitude >> None)
 g = geocoders.GoogleV3()
 
-success = 0
 error = 0
 
-for playground in p:
-    time.sleep(0.25)
+with open('data/playgrounds.csv', 'rb') as readfile:
+    read_csv = csv.reader(readfile)
 
-    try:
-        place, (lat, lng) = g.geocode('%s %s %s %s' % (playground.address, playground.city, playground.state, playground.zip_code))
-        print place, lat, lng
-        success += 1
+    with open('data/playgrounds_geocoded.csv', 'wb') as writefile:
+        write_csv = csv.writer(writefile)
 
-    except:
-        print '\t%s %s %s %s' % (playground.address, playground.city, playground.state, playground.zip_code)
-        error += 1
+        for playground in Playground.select().where(Playground.longitude >> None):
+            time.sleep(0.25)
 
-    playground.latitude = lat
-    playground.longitude = lng
-    playground.save()
+            try:
+                place, (lat, lng) = g.geocode('%s %s %s %s' % (playground.address, playground.city, playground.state, playground.zip_code))
 
-print success, error
+            except:
+                print '\t%s %s %s %s' % (playground.address, playground.city, playground.state, playground.zip_code)
+                error += 1
+
+            playground.latitude = lat
+            playground.longitude = lng
+            playground.save()
+
+            for row in read_csv:
+                if row[1] == playground.name:
+                    row[9] = lat
+                    row[10] = lng
+
+                write_csv.writerow(row)
+
+print error
