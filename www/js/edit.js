f3d158e04a4511550810814f5621d5851597145c
geraldarthur <gerald.a.rich@gmail.com>
2013-07-01 15:02:44 -0400

    clean up edit.js, add marker div

--- www/js/edit.js
+++ www/js/edit.js
@@ -2,8 +2,13 @@ var $edit_map = null;
 
 $(function() {
    $edit_map = $('#edit-map');
-   $edit_map.width(300);
-   $edit_map.height(500);
-   edit_map = L.mapbox.map('edit-map', 'examples.map-y7l23tes')
-      .setView([37.9, -77], 5);
+   $edit_map.width('100%');
+   $edit_map.height('250');
+   map = L.map('edit-map').setView([38.9, -77], 7);
+   map_layer = L.mapbox.tileLayer('geraldrich.map-wow338yo', {
+       detectRetina: true,
+       retinaVersion: 'geraldrich.map-y6mt2diq'
+   }).addTo(map);
+   grid_layer = L.mapbox.gridLayer('geraldricamples.map-wow338yo').addTo(map);
+   map.addControl(L.mapbox.gridControl(grid_layer));
 });
