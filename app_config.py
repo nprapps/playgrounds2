710139b7e6279a402726cdbde4b4915f85eae1c8
Jeremy Bowers <jeremyjbowers@gmail.com>
2013-07-01 15:23:10 -0400

    Save Gerald's inbox. Prep for playgrounds@npr.org release.

--- app_config.py
+++ app_config.py
@@ -101,13 +101,12 @@ CLOUD_SEARCH_REGION = 'us-west-2'
 CLOUD_SEARCH_INDEX_NAME = 'nprapps-playgrounds'
 CLOUD_SEARCH_DOMAIN = 'search-nprapps-playgrounds-ujjvpbsloblpc625kcbhogeb3u.us-west-2.cloudsearch.amazonaws.com'
 CLOUD_SEARCH_DOC_DOMAIN = 'doc-nprapps-playgrounds-ujjvpbsloblpc625kcbhogeb3u.us-west-2.cloudsearch.amazonaws.com'
-CLOUD_SEARCH_DEFAULT_SEARCH_FIELD = '' # empty string searches all text fields
+CLOUD_SEARCH_DEFAULT_SEARCH_FIELD = ''  # empty string searches all text fields
 CLOUD_SEARCH_DEG_OFFSET = 180
 CLOUD_SEARCH_DEG_SCALE = 10000
 
 ADMIN_EMAILS = [
-    'jbowers@npr.org',
-    'grich@npr.org'
+    'jbowers@npr.org'
 ]
 
 FEATURES = {
