9a56851bd5889201780b4d6d975b4accaf78bd68
geraldarthur <gerald.a.rich@gmail.com>
2013-07-01 15:35:14 -0400

    move edit map styles to app.less, add static marker over map

--- www/js/edit.js
+++ www/js/edit.js
@@ -1,14 +1,11 @@
 var $edit_map = null;
 
 $(function() {
-   $edit_map = $('#edit-map');
-   $edit_map.width('100%');
-   $edit_map.height('250');
    map = L.map('edit-map').setView([38.9, -77], 7);
    map_layer = L.mapbox.tileLayer('geraldrich.map-wow338yo', {
        detectRetina: true,
        retinaVersion: 'geraldrich.map-y6mt2diq'
    }).addTo(map);
-   grid_layer = L.mapbox.gridLayer('geraldricamples.map-wow338yo').addTo(map);
+   grid_layer = L.mapbox.gridLayer('geraldrich.map-wow338yo').addTo(map);
    map.addControl(L.mapbox.gridControl(grid_layer));
 });
