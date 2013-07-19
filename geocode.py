1b8e3f0cf378cca7610495a56eaf081a38f5228c
geraldarthur <gerald.a.rich@gmail.com>
2013-07-19 17:52:23 -0400

    track geocode successes and fails

--- geocode.py
+++ geocode.py
@@ -7,18 +7,20 @@ from geopy import geocoders
 g = geocoders.GoogleV3()
 
 error = 0
+coded = 0
 
-with open('data/playgrounds.csv', 'rb') as readfile:
+with open('data/playgrounds.csv', 'r+b') as readfile:
     read_csv = csv.reader(readfile)
 
-    with open('data/playgrounds_geocoded.csv', 'wb') as writefile:
+    with open('data/playgrounds_geocoded.csv', 'w+b') as writefile:
         write_csv = csv.writer(writefile)
 
-        for playground in Playground.select().where(Playground.longitude >> None):
+        for playground in Playground.select().where(Playground.latitude >> None or Playground.longitude >> None):
             time.sleep(0.25)
 
             try:
                 place, (lat, lng) = g.geocode('%s %s %s %s' % (playground.address, playground.city, playground.state, playground.zip_code))
+                coded += 1
 
             except:
                 print '\t%s %s %s %s' % (playground.address, playground.city, playground.state, playground.zip_code)
@@ -35,4 +37,5 @@ with open('data/playgrounds.csv', 'rb') as readfile:
 
                 write_csv.writerow(row)
 
+print coded
 print error
