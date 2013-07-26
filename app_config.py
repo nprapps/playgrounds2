4b9538729b0278c38475cb5d8b801ffdbadf8147
Jeremy Bowers <jeremyjbowers@gmail.com>
2013-07-26 13:24:31 -0400

    Work on geocoder.py, updates spreadsheet location in app_config, updated csv.

--- app_config.py
+++ app_config.py
@@ -91,7 +91,7 @@ DISQUS_SHORTNAME = 'npr-playgrounds2'
 """
 Application specific
 """
-DATA_GOOGLE_DOC_KEY = '0AlXMOHKxzQVRdGxsTklpUnBHV3NSRHFZaDVFWnQ5VVE'
+DATA_GOOGLE_DOC_KEY = '0Antez86oOXPndGhEdGV6Qm9oMTQ0MFdVZTFnazRYaEE'
 DATA_URL = 'https://docs.google.com/spreadsheet/pub?key=%s&single=true&gid=0&output=csv' % DATA_GOOGLE_DOC_KEY
 
 MAPBOX_BASE_LAYER = 'npr.map-s5q5dags'
