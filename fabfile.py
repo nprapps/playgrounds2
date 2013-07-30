24ea5571cdb84bcd56e2c027e92a5cf68d2bf294
Jeremy Bowers <jeremyjbowers@gmail.com>
2013-07-30 15:43:52 -0400

    Maybe a utility function?

--- fabfile.py
+++ fabfile.py
@@ -127,6 +127,10 @@ def app_config_js():
 def render_playgrounds():
     data.render_playgrounds()
 
+def remote(fab_command):
+    require('settings', provided_by=[production, staging])
+    run('cd %s && bash cron.sh fab $DEPLOYMENT_TARGET %s' % (env.repo_path, fab_command))
+
 def remote_deploy_playgrounds():
     require('settings', provided_by=[production, staging])
     run('cd %(repo_path)s && bash cron.sh fab $DEPLOYMENT_TARGET deploy_playgrounds' % env)
