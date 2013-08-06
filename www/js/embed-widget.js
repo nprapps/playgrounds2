6233ad49e9c051811cabe9b831285438d2e5e7fb
Alyson Hurt <ahurt@npr.org>
2013-08-06 14:24:30 -0400

    set up widget analytics/metrics code. closes #604

--- www/js/embed-widget.js
+++ www/js/embed-widget.js
@@ -6,19 +6,18 @@ var gaJsHost = (("https:" == document.location.protocol) ? "https://ssl." : "htt
 document.write(unescape("%3Cscript src=\'" + gaJsHost + "google-analytics.com/ga.js\' type=\'text/javascript\'%3E%3C/script%3E"));
 
 try {
-        var pageTracker = _gat._getTracker("UA-5828686-3");
-        pageTracker._setDomainName("none");
-        pageTracker._setAllowLinker(true);
-        pageTracker._setCustomVar(1,"Module","TODO",3);
-        pageTracker._trackPageview();
+	var pageTracker = _gat._getTracker("UA-5828686-3");
+	pageTracker._setDomainName("none");
+	pageTracker._setAllowLinker(true);
+	pageTracker._setCustomVar(1,"Module","accessible_playgrounds",3);
+	pageTracker._trackPageview();
 } catch(err) {}
-
 
 /* check for user-defined width */
 try {
 	if (nprapps_widget_width) {}
 } catch (err) {
-	nprapps_widget_width = '300';
+	nprapps_widget_width = '600';
 }
 
 try {
@@ -28,7 +27,5 @@ try {
 }
 
 document.write(
-'<script type="text/javascript">window.vsitag = {"imp":"TODO"};</script>',
-'<script type="text/javascript" language="javascript" src="http://www.npr.org/include/javascript/zigi.js"></script>',
-'<iframe src="widget.html?station=' + window.station + '" width="' + nprapps_widget_width + '" height="' + nprapps_widget_height + '" scrolling="auto" marginheight="0" marginwidth="0" frameborder="0" style="border: 1px solid #CCC;" ></iframe>',
+'<iframe src="widget.html?station=' + window.station + '" width="' + nprapps_widget_width + '" height="' + nprapps_widget_height + '" scrolling="auto" marginheight="0" marginwidth="0" frameborder="0"></iframe>',
 '');
