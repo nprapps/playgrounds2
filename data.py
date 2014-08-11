f9ffc168c6dfcf1936f5557b36fdc413e4820a3e
Christopher Groskopf <staringmonkey@gmail.com>
2014-08-11 12:00:19 -0500

    Never update active key.

--- data.py
+++ data.py
@@ -348,7 +348,7 @@ def process_update(record):
     for key, value in record['playground'].items():
 
         # Ignore some keys because they aren't really what we want to update.
-        if key not in ['id', 'features']:
+        if key not in ['id', 'active', 'features']:
 
             # Update the record_dict with our new key/value pair.
             record_dict[key] = value
