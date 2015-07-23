03d5af1ec41559cda749c250e15477333c2953a4
David Eads <davideads@gmail.com>
2015-07-23 17:46:41 -0400

    hard reset services no matter what (temporary)

--- fabfile.py
+++ fabfile.py
@@ -378,7 +378,8 @@ def deploy_confs():
             a = local('md5 -q %s' % local_path, capture=True)
             b = run('md5sum %s' % remote_path).split()[0]
 
-            if a != b:
+            #if a != b:
+            if True:
                 put(local_path, remote_path, use_sudo=True)
 
                 if service == 'nginx':
